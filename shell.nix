2de9e817fac0e54e772684479c2c3c5462dd3c4b
Stefan Junker <mail@stefanjunker.de>
2021-08-18 16:36:19 +0200

    WIP nix expressions

--- shell.nix
+++ shell.nix
@@ -1,62 +1,119 @@
-let 
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/3e94163765975f35f7d8ec509b33c3da52661bd1.tar.gz";
-    sha256 = "07sl281r29ygh54dxys1qpjvlvmnh7iv1ppf79fbki96dj9ip7d2";
-  };
-  holonix = import (holonixPath) {
-    includeHolochainBinaries = true;
-    holochainVersionId = "custom";
-    
-    holochainVersion = { 
-     rev = "6b34b1797042b72aa7ae80364d3616a321924f75";
-     sha256 = "sha256:0ky2aq367ava09w19371fa77mp23kr99vp26g5gncm6nwjbazx89";
-     cargoSha256 = "sha256:07gdvccvjbg5zina751r8d8ga87pb84ss2a5ib453ykwparr53i3";
-     bins = {
-       holochain = "holochain";
-       hc = "hc";
-     };
+{ sources ? import ./nix/sources.nix
+, nixpkgs ? sources.nixpkgs
+, holochainNixpkgsPath ? sources.holochain-nixpkgs
+}:
+
+# DEBUG LOG:
+# after an initial and failing `yarn run build` this command reveals more details about what fails:
+# nix-shell --run "cd src-tauri/target/release/bundle/appimage; env NO_STRIP=1 OUTPUT=holochain-launcher_0.2.0_amd64.AppImage ./linuxdeploy-x86_64.AppImage --appdir holochain-launcher.AppDir --plugin gtk --output appimage"
+
+let
+  pkgs = import nixpkgs {};
+
+  nodejs_16_7_0 =
+    let
+      buildNodejs = pkgs.callPackage "${sources.nixpkgs-master}/pkgs/development/web/nodejs/nodejs.nix" {
+        python = pkgs.python3;
+      };
+    in buildNodejs {
+      enableNpm = true;
+      version = "16.7.0";
+      sha256 = "0drd7zyadjrhng9k0mspz456j3pmr7kli5dd0kx8grbqsgxzv1gs";
+      patches = [ "${sources.nixpkgs-master}/pkgs/development/web/nodejs/disable-darwin-v8-system-instrumentation.patch" ];
     };
-    holochainOtherDepsNames = ["lair-keystore"];
-  };
-  nixpkgs = holonix.pkgs;
-in nixpkgs.mkShell {
-  inputsFrom = [ holonix.main ];
-  buildInputs = with nixpkgs; [
-    caddy
-    glib
+
+  default = import ./default.nix { inherit sources nixpkgs holochainNixpkgsPath; };
+  inherit (default)
+    holochainNixpkgs
+    ;
+
+  myglib =
+    (
+      pkgs.runCommand "glibWithDev" {} ''
+        mkdir $out
+        cp -r ${pkgs.glib}/* $out/
+        cp -r ${pkgs.glib.out}/* $out/
+        find $out -type d -exec chmod +w {} +
+        cp -r ${pkgs.glib.dev}/* $out/
+        find $out -type d -exec chmod -w {} +
+
+        for f in $out/lib/pkgconfig/*.pc; do
+          substituteInPlace $f \
+            --replace ${pkgs.glib.out} $out
+        done
+      ''
+    )
+    ;
+
+  libs = with pkgs; [
+    myglib
     cairo
     pango
     atk
     gdk-pixbuf
     libsoup
     gtk3
-    pkgconfig
     webkitgtk
     gtksourceview3
     llvmPackages.libclang
     llvmPackages.libcxxClang
-    clang
-    zlib
     libappindicator
-    squashfsTools
+
+    openssl
   ];
 
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    myglib
+  ];
 
-  shellHook = ''
-    export LIBCLANG_PATH="${nixpkgs.llvmPackages.libclang}/lib";
+  packages = with pkgs; [
+    myglib
+    clang
+    squashfsTools
+    pkgs.rust.packages.stable.rustc
+    pkgs.rust.packages.stable.cargo
+    (yarn.override { nodejs = nodejs_16_7_0; })
+  ] ++ libs;
+  # TODO:
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+    (with pkgs; [
+      zlib
+      fuse
+      harfbuzz
+      libglvnd
+      xlibs.libSM
+      xlibs.libX11
+      xlibs.libICE
+      libgccjit
+      libgpgerror
+      fontconfig
+      freetype
+      xorg.libxcb
+      fribidi
+    ])
+    ++ libs
+    );
+
+  shellHook = let
+    srcTauriDirBins = "${builtins.toString ./.}/src-tauri/bins";
+  in with pkgs; ''
+    export LIBCLANG_PATH="${llvmPackages.libclang}/lib";
     unset SOURCE_DATE_EPOCH;
 
-    rm -rf src-tauri/bins/
-    mkdir src-tauri/bins
+    rm -rf ${srcTauriDirBins}
+    mkdir -p ${srcTauriDirBins}
 
-    HOLOCHAIN_PATH=$(which holochain)
-    cp $HOLOCHAIN_PATH src-tauri/bins/holochain-x86_64-unknown-linux-gnu
+    # FIXME: what about other architectures than x86_64-unknown-linux-gnu?
+    cp ${holochainNixpkgs.packages.holochainAllBinariesWithDeps.main.holochain}/bin/holochain ${srcTauriDirBins}/holochain-x86_64-unknown-linux-gnu
+    cp ${holochainNixpkgs.packages.holochainAllBinariesWithDeps.main.lair-keystore}/bin/lair-keystore ${srcTauriDirBins}/lair-keystore-x86_64-unknown-linux-gnu
+    cp ${caddy}/bin/caddy ${srcTauriDirBins}/caddy-x86_64-unknown-linux-gnu
 
-    LAIR_PATH=$(which lair-keystore)
-    cp $LAIR_PATH src-tauri/bins/lair-keystore-x86_64-unknown-linux-gnu
-
-    CADDY_PATH=$(which caddy)
-    cp $CADDY_PATH src-tauri/bins/caddy-x86_64-unknown-linux-gnu
-
+    # TODO: tauri tries to access these binaries in write-mode for some reason. find out why
+    chmod +w \
+      ${srcTauriDirBins}/holochain-x86_64-unknown-linux-gnu \
+      ${srcTauriDirBins}/lair-keystore-x86_64-unknown-linux-gnu \
+      ${srcTauriDirBins}/caddy-x86_64-unknown-linux-gnu
   '';
 }
